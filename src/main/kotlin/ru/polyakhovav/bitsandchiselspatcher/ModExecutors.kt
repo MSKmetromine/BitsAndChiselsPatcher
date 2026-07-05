bada5fead5e08dddc93c44c027c26668253c532f
A. Polyakhov <a.polyakhov@mail.ru>
2026-07-05 11:49:04 -0400

    Reduce the number of used processors.

--- src/main/kotlin/ru/polyakhovav/bitsandchiselspatcher/ModExecutors.kt
+++ src/main/kotlin/ru/polyakhovav/bitsandchiselspatcher/ModExecutors.kt
@@ -1,7 +1,10 @@
 package ru.polyakhovav.bitsandchiselspatcher
 
 import java.util.concurrent.Executors
+import kotlin.math.max
 
 object ModExecutors {
-    val EXECUTOR = Executors.newWorkStealingPool()
+    val EXECUTOR = Executors.newWorkStealingPool(
+        max(1, Runtime.getRuntime().availableProcessors() / 2)
+    )
 }
