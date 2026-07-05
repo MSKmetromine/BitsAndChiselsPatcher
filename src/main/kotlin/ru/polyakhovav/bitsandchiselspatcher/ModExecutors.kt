c1dc9dec27aa45bdefc19d01f106a2ad79174a33
A. Polyakhov <a.polyakhov@mail.ru>
2026-07-04 20:45:37 -0400

    Use a work stealing pool instead of a single thread.

--- src/main/kotlin/ru/polyakhovav/bitsandchiselspatcher/ModExecutors.kt
+++ src/main/kotlin/ru/polyakhovav/bitsandchiselspatcher/ModExecutors.kt
@@ -3,5 +3,5 @@ package ru.polyakhovav.bitsandchiselspatcher
 import java.util.concurrent.Executors
 
 object ModExecutors {
-    val EXECUTOR = Executors.newSingleThreadExecutor()
+    val EXECUTOR = Executors.newWorkStealingPool()
 }
